2e66afcebaa02f8f6ae12be466a445cf347795aa
Dennis <dennisdobra@gmail.com>
2024-10-01 01:06:01 +0300

    removed .gitignore file from second stage

---
+++ Stage 2/src/fileio/input/UserInput.java
@@ -0,0 +1,52 @@
+package fileio.input;
+
+public final class UserInput {
+    private String username;
+    private int age;
+    private String city;
+    private String type;
+
+    public UserInput() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(final String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(final int age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(final String city) {
+        this.city = city;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(final String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "UserInput{"
+                + "username='" + username + '\''
+                + ", age=" + age
+                + ", city='" + city + '\''
+                + '}';
+    }
+}
